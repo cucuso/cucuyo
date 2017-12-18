de243bb0ccf06a527dea209bfc00386ba134139e
eduardo.eljaiek <eduardo.eljaiek@gmail.com>
2017-12-18 09:01:03 -0500

    [*] Improve code cognitive complexity

--- src/main/java/com/cucuyo/data/DataAdPropertiesRepositoryImpl.java
+++ src/main/java/com/cucuyo/data/DataAdPropertiesRepositoryImpl.java
@@ -5,11 +5,13 @@ import com.cucuyo.core.domain.PageRequest;
 import com.datastax.driver.core.PagingState;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.SimpleStatement;
+import com.google.common.base.Strings;
 import com.stratio.cassandra.lucene.builder.Builder;
+import com.stratio.cassandra.lucene.builder.search.condition.BooleanCondition;
+import com.stratio.cassandra.lucene.builder.search.condition.Condition;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.cassandra.core.CassandraTemplate;
 
 import java.util.ArrayList;
@@ -21,26 +23,24 @@ import static com.cucuyo.data.AdPropertiesEntity.getTableName;
 @RequiredArgsConstructor
 class DataAdPropertiesRepositoryImpl implements PagingDataAdPropertiesRepository {
 
-    private static final String SEARCH_BASE_QUERY = "SELECT * FROM %s.%s WHERE expr(ad_properties_search_index, '%s');";
-    private static final String SEARCH_COUNT_BASE_QUERY = "SELECT COUNT(*) FROM %s.%s WHERE expr(ad_properties_search_index, '%s');";
-
+    private static final String SEARCH_BASE_QUERY = "SELECT * FROM %s WHERE expr(ad_properties_search_index, '%s');";
+    private static final String SEARCH_COUNT_BASE_QUERY = "SELECT COUNT(*) FROM %s WHERE expr(ad_properties_search_index, '%s');";
     private final CassandraTemplate cassandraTemplate;
     private final AdPropertiesEntityMapper mapper;
-    @Value("${spring.data.cassandra.keyspace-name}")
-    private String keyspace;
 
-    private static String appendWildcards(String text) {
-        return "*" + text + "*";
-    }
+
 
     @Override
     public Page<AdPropertiesEntity> findAllByFullText(@NonNull String text, @NonNull PageRequest pageRequest) {
         val descCondition = Builder.wildcard("description", appendWildcards(text));
         val filters = Builder.search().filter(descCondition).build();
-        val args = new Object[]{keyspace, getTableName(), filters};
-        val query = String.format(SEARCH_BASE_QUERY, args);
-        val totalElementsQuery = String.format(SEARCH_COUNT_BASE_QUERY, args);
+        val query = String.format(SEARCH_BASE_QUERY, getTableName(), filters);
+        val totalElementsQuery = String.format(SEARCH_COUNT_BASE_QUERY, getTableName(), filters);
         return execute(query, totalElementsQuery, pageRequest);
+    }
+
+    private static String appendWildcards(String text) {
+        return "*" + text + "*";
     }
 
     private Page<AdPropertiesEntity> execute(String query, String totalElementsQuery, PageRequest pageRequest) {
@@ -69,19 +69,27 @@ class DataAdPropertiesRepositoryImpl implements PagingDataAdPropertiesRepository
     }
 
     @Override
-    public Page<AdPropertiesEntity> findAllByFullTextAndPriceBetween(@NonNull String text, @NonNull double from, @NonNull double to, @NonNull PageRequest pageRequest) {
-        val descCondition = Builder.wildcard("description", appendWildcards(text));
-        val priceCondition = Builder
+    public Page<AdPropertiesEntity> findAllByFullTextAndPriceBetween(String text, @NonNull double from, @NonNull double to, @NonNull PageRequest pageRequest) {
+        val condition = getAdvancedFilterCondition(text, from, to);
+        val filters = Builder.search().filter(condition).build();
+        val query = String.format(SEARCH_BASE_QUERY, getTableName(), filters);
+        val totalElementsQuery = String.format(SEARCH_COUNT_BASE_QUERY, getTableName(), filters);
+        return execute(query, totalElementsQuery, pageRequest);
+    }
+
+    private static BooleanCondition getAdvancedFilterCondition(String text, @NonNull double from, @NonNull double to) {
+        val conditions = new Condition[2];
+        conditions[0] = Builder
                 .range("price")
                 .lower(from)
                 .includeLower(true)
                 .upper(to)
                 .includeUpper(true);
-        val andCondition = Builder.bool().must(descCondition, priceCondition);
-        val filters = Builder.search().filter(andCondition).build();
-        val args = new Object[]{keyspace, getTableName(), filters};
-        val query = String.format(SEARCH_BASE_QUERY, args);
-        val totalElementsQuery = String.format(SEARCH_COUNT_BASE_QUERY, args);
-        return execute(query, totalElementsQuery, pageRequest);
+
+        if (!Strings.isNullOrEmpty(text)) {
+            conditions[1] = Builder.wildcard("description", appendWildcards(text));
+        }
+
+        return Builder.bool().must(conditions);
     }
 }
