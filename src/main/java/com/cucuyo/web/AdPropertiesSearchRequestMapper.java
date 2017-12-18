c46d631356ac0ca9f62f99bdb8e2e83df567c79d
eduardo.eljaiek <eduardo.eljaiek@gmail.com>
2017-12-18 01:06:49 -0500

    [*] Fixed advanced search not working because incorrect price range in request mapping

--- src/main/java/com/cucuyo/web/AdPropertiesSearchRequestMapper.java
+++ src/main/java/com/cucuyo/web/AdPropertiesSearchRequestMapper.java
@@ -3,10 +3,16 @@ package com.cucuyo.web;
 import com.cucuyo.core.domain.AdPropertiesSearch;
 import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper
 @DecoratedWith(AdPropertiesSearchRequestMapperDecorator.class)
 public interface AdPropertiesSearchRequestMapper {
 
+    @Mappings({
+            @Mapping(source = "from", target = "fromPrice"),
+            @Mapping(source = "to", target = "toPrice")
+    })
     AdPropertiesSearch asAdPropertiesSearch(AdPropertiesSearchRequest request);
 }
